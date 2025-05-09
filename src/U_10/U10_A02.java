cc800775af6f8c04da894aeecd85294d2c52903a
Jesus Sanchez de la Riva <sanchezdelariva.jesus@jgvaldemora.org>
2025-05-09 08:35:20 +0200

    Unidad 10, Actividad 2

---
+++ src/U_10/U10_A02.java
@@ -0,0 +1,20 @@
+//Jesús Sánchez de la Riva
+package U_10;
+import java.io.*;
+public class U10_A02 {
+    public static void main(String[] args){
+        
+        
+        try{
+            FileReader in=new FileReader("C:\\Users\\jesus\\OneDrive\\Documentos\\NetBeansProjects\\PROG-24_25\\src\\U_10\\Main.java");
+            int letras;
+            while((letras=in.read())!= -1){
+                System.out.print((char)letras);
+            }
+            in.close();
+        }catch(IOException ex){
+            System.out.println("Ha ocurrido un error al leer: "+ex.getMessage());
+        }
+    }
+    
+}
