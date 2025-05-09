85ac37acc46ea7c3878ab6654cb84ad8bb2d6d4e
Jesus Sanchez de la Riva <sanchezdelariva.jesus@jgvaldemora.org>
2025-05-09 08:52:03 +0200

    Unidad 10, Actividad 2

--- src/U_10/U10_A02.java
+++ src/U_10/U10_A02.java
@@ -3,8 +3,9 @@ package U_10;
 import java.io.*;
 public class U10_A02 {
     public static void main(String[] args){
+        FileReader in = null;
         try{
-            FileReader in=new FileReader("C:\\Users\\jesus\\OneDrive\\Documentos\\NetBeansProjects\\PROG-24_25\\src\\U_10\\Main.java");
+            in=new FileReader("C:\\Users\\jesus\\OneDrive\\Documentos\\NetBeansProjects\\PROG-24_25\\src\\U_10\\Main.java");
             int letras;
             while((letras=in.read())!= -1){
                 System.out.print((char)letras);
@@ -12,7 +13,14 @@ public class U10_A02 {
             in.close();
         }catch(IOException ex){
             System.out.println("Ha ocurrido un error al leer: "+ex.getMessage());
+        }finally{
+            try{
+                if(in!=null){
+                    in.close();
+                }
+            }catch(IOException ex){
+                System.out.println("No se ha podido cerrar el archivo: " + ex.getMessage());
+            }
         }
     }
-    
 }
